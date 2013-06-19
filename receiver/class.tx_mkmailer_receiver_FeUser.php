5127e3858e7bfb14c7dbd492d0218267340cedf9
Thomas Reuleke <thomas.reuleke@das-medienkombinat.de>
2013-06-19 11:12:39 +0200

    load fuer tx_mkmailer_receiver_BaseTemplate korrigiert

--- receiver/class.tx_mkmailer_receiver_FeUser.php
+++ receiver/class.tx_mkmailer_receiver_FeUser.php
@@ -22,7 +22,7 @@
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 
-tx_rnbase::load('tx_mkmailer_receiver_Base');
+tx_rnbase::load('tx_mkmailer_receiver_BaseTemplate');
 /**
  * Implementierung für einen Mailempfänger vom Typ FeUser.
  *
