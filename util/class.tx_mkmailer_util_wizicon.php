eddbb9f11b711a6300f70d3e0e98529193d0b18f
Eric Hertwig <eric.hertwig@dmk-ebusiness.de>
2015-12-18 13:30:44 +0100

    [Bugfix] Show default labels for wizicon if label is not in languageFile - using tx_rnbase_util_Wizicon

--- util/class.tx_mkmailer_util_wizicon.php
+++ util/class.tx_mkmailer_util_wizicon.php
@@ -1,75 +1,65 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2009 Rene Nitzsche (rene@system25.de)
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2009 Rene Nitzsche (rene@system25.de)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 /**
  *
  * tx_mkmailer_util_wizicon
  *
- * @package 		TYPO3
- * @subpackage	 	mkmailer
- * @author 			René Nitzsche <dev@dmk-ebusiness.de>
- * @license 		http://www.gnu.org/licenses/lgpl.html
- * 					GNU Lesser General Public License, version 3 or later
+ * @package        TYPO3
+ * @subpackage     mkmailer
+ * @author         René Nitzsche <dev@dmk-ebusiness.de>
+ * @author         Eric Hertwig <dev@dmk-ebusiness.de>
+ * @license        http://www.gnu.org/licenses/lgpl.html
+ *                 GNU Lesser General Public License, version 3 or later
  */
-class tx_mkmailer_util_wizicon {
 
-	/**
-	 * Adds the Netfewo plugin wizard icon
-	 *
-	 * @param array Input array with wizard items for plugins
-	 * @return array Modified input array, having the items for Netfewo plugins added.
-	 */
-	function proc($wizardItems)	{
-		global $LANG;
+tx_rnbase::load( 'tx_rnbase_util_Extensions' );
+tx_rnbase::load( 'tx_rnbase_util_Wizicon' );
 
-		$LL = $this->includeLocalLang();
-		$wizardItems['plugins_tx_mkmailer'] = array(
-			'icon'=>t3lib_extMgm::extRelPath('mkmailer').'/ext_icon.gif',
-			'title'=>$LANG->getLLL('plugin.mkmailer.label',$LL),
-			'description'=>$LANG->getLLL('plugin.mkmailer.description',$LL),
-			'params'=>'&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=tx_mkmailer'
-		);
-
-		return $wizardItems;
-	}
+class tx_mkmailer_util_wizicon extends tx_rnbase_util_Wizicon {
 
 	/**
 	 * @return array
 	 */
-	function includeLocalLang()	{
-		$llFile = t3lib_extMgm::extPath('mkmailer').'locallang_db.xml';
-		if (tx_rnbase_util_TYPO3::isTYPO46OrHigher()) {
-			$llXmlParser = tx_rnbase::makeInstance('t3lib_l10n_parser_Llxml');
-			$LOCAL_LANG =  $llXmlParser->getParsedData($llFile, $GLOBALS['LANG']->lang);
-		}
-		else {
-			$LOCAL_LANG = t3lib_div::readLLXMLfile($llFile, $GLOBALS['LANG']->lang);
-		}
-		return $LOCAL_LANG;
+	protected function getPluginData() {
+		return array(
+			'plugins_tx_mksearch' => array(
+				'icon'        => tx_rnbase_util_Extensions::extRelPath( 'mkmailer' ) . 'ext_icon.gif',
+				'title'       => 'plugin.mkmailer.label',
+				'description' => 'plugin.mkmailer.description'
+			)
+		);
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getLLFile() {
+		return tx_rnbase_util_Extensions::extPath( 'mkmailer' ) . 'locallang_db.xml';
 	}
 }
 
-if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/mkmailer/util/class.tx_mkmailer_util_wizicon.php'])	{
-	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/mkmailer/util/class.tx_mkmailer_util_wizicon.php']);
+if ( defined( 'TYPO3_MODE' ) && $GLOBALS['TYPO3_CONF_VARS'][ TYPO3_MODE ]['XCLASS']['ext/mkmailer/util/class.tx_mkmailer_util_wizicon.php'] ) {
+	include_once( $GLOBALS['TYPO3_CONF_VARS'][ TYPO3_MODE ]['XCLASS']['ext/mkmailer/util/class.tx_mkmailer_util_wizicon.php'] );
 }
