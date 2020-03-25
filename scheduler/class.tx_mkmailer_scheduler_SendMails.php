8ea7e1f0f74d2d85225bdb4e97356f3ffc92b772
Patrik Garten <patrik.garten@dmk-ebusiness.com>
2020-03-25 19:37:53 +0100

    [FIX] Exception on site scheduler trigger
    Fix exception which triggered while trying to run the scheduler in TYPO3 v9 or higher.
    
    UNCAUGHT EXCEPTION FOR VIEW: tx_mkmailer_scheduler_SendMails
    Message: InvalidArgumentException: Unable to parse URI: http:///index.php?id=52
    Stacktrace: InvalidArgumentException: Unable to parse URI: http:///index.php?id=52 in /var/www/html/vendor/guzzlehttp/psr7/src/Uri.php:71

--- scheduler/class.tx_mkmailer_scheduler_SendMails.php
+++ scheduler/class.tx_mkmailer_scheduler_SendMails.php
@@ -111,15 +111,22 @@ class tx_mkmailer_scheduler_SendMails extends tx_mklib_scheduler_Generic
     protected function getCronpageUrl()
     {
         $pageUid = $this->getCronPageId();
-        // seems like we have an alias
-        if (!Tx_Rnbase_Utility_Strings::isInteger($pageUid)) {
-            $pageUid = tx_rnbase_util_TYPO3::getSysPage()->getPageIdFromAlias($pageUid);
-        }
-        $domain = $GLOBALS['TSFE']->getDomainNameForPid($pageUid);
         $user = $this->getOption('user');
         $pwd = $this->getOption('passwd');
         $auth = ($user && $pwd) ? $user . ':' . $pwd . '@' : '';
         $protocol = $this->getProtocol();
+
+        if (tx_rnbase_util_TYPO3::isTYPO90OrHigher()) {
+            /** @var \TYPO3\CMS\Core\Http\ServerRequest $request */
+            $request = $GLOBALS['TYPO3_REQUEST'];
+            $domain = $request->getServerParams()['SERVER_NAME'];
+        } else {
+            // seems like we have an alias
+            if ( !Tx_Rnbase_Utility_Strings::isInteger($pageUid)) {
+                $pageUid = tx_rnbase_util_TYPO3::getSysPage()->getPageIdFromAlias($pageUid);
+            }
+            $domain = $GLOBALS['TSFE']->getDomainNameForPid($pageUid);
+        }
 
         return sprintf(
             '%1$s://%2$s%3$s/index.php?id=%4$s',
